853c733315ab44c333e9d3179c7ae1521f4c8d29
Majeed <aabdulmajeed16@gmail.com>
2023-07-05 23:39:15 +0300

    disable login

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use App\Models\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
@@ -73,5 +74,10 @@ class RegisterController extends Controller
             'password' => Hash::make($data['password']),
             'order' => rand(1, 200),
         ]);
+    }
+
+    public function showRegistrationForm()
+    {
+        return redirect('home');
     }
 }
